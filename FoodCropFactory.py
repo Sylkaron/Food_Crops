c06af4c98b2aee4368a92318dfb7f691ca09eb71
maxim <maxim.mangematin--mathey@mines-ales.org>
2021-10-20 17:32:22 +0200

    FoodCrop fait

---
+++ FoodCropFactory.py
@@ -0,0 +1,33 @@
+import Unit, indicator, Measurement
+
+class FoodCropFactory():
+
+    def __init__(self):
+        pass
+
+    def createVolume(self, id:int):
+        return Volume(id)
+
+    def createPrice(self, id:int):
+        return Price(id)
+
+    def createWeight(self, id:int, weight:float):
+        return Weight(id,weight)
+
+    def createSurface(self, id:int):
+        return Surface(id)
+
+    def createCount(self,id:int, what:str):
+        return Count(id, what)
+
+    def createRatio(self, id:int):
+        return Ratio(id)
+
+    def createCommodity(self, group:CommodityGroup, id:int, name:str):
+        new Commodity(group, id, name)
+
+    def createIndicator(self, id:int, frequency:int, freqDesc:str, geogLocation:str, indicatorGroup:IndicatorGroup, unit:Unit ):
+        return Indicator(id, frequency, freqDesc, geogLocation, indicatorGroup, unit)
+
+    def createMeasurement(self, id:int, year:int, value:float, timeperiodId:int, timeperiodDesc:str, commodity:Commodity, indicator:Indicator):
+        return Measurement(id, year, value, timeperiodId, timeperiodDesc, commodity, indicator)
--- Indicator.py
+++ Indicator.py
@@ -3,18 +3,27 @@ import Describable
 
 class IndicatorGroup(Enum):
     EXPORTS_AND_IMPORTS = 1
+    SUPPLY_AND_USE = 2
+    PRICES = 3
+    FEED_PRICES_RATIOS = 4
+    QUANTITIES_FED=5
+    TRANSPORTATION=6
+    ANIMAL_UNIT_INDEXES = 7
 
 
 
-class Indicator():
+class Indicator(Describable):
 
-    def __init__(self,id: str,frequency: int,frequencyDesc:str,geogLocation:str):
+    def __init__(self,id: str,frequency: int,frequencyDesc:str,geogLocation:str,indicatorGroup:IndicatorGroup,unit:Unit):
+        super(Indicator, self).__init__()
         self.id = id
         self.frequency = frequency
         self.frequencyDesc = frequencyDesc
         self.geogLocation = geogLocation
+        self.indicatorGroup = indicatorGroup
+        self.unit = unit
+        
 
-    def Indicator(id:int,frequency:int,freqDesc:str,geogLocation:str,indicatorGroup:IndicatorGroup,unit:Unit):
 
 
 
