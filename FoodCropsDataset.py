ab0b1f1e4d083ed1e33e258649d998d5b6af43a3
maxim <maxim.mangematin--mathey@mines-ales.org>
2021-10-20 18:00:01 +0200

    debutLoad

--- FoodCropsDataset.py
+++ FoodCropsDataset.py
@@ -1,7 +1,16 @@
+import pandas
+
 class FoodCropsDataset:
     def __init__(self):
 
     def load(self, datasetPath: str):
+        dataframe = pandas.read_csv(datasetPath)
+        F = FoodCropFactory()
+        for index, row in dataframe.iterrows():
+            Prix = row["SC_Group_ID"] * 1000
+            descPrix = row["SC_Group_Desc"]
+            CommodityID =  row["SC_GroupCommod_ID"]
+            CommodityDesc = row["SC_GroupCommod_Desc"]
 
     def findMeasurements(self, commodityType: CommodityType = None, indicatorGroup: IndicatorGroup = None,
                          geographicalLocation: str = None, unit: Unit = None):
