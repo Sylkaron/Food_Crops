2613270b1189ab7a72d5be8e7b7f64ed33bd504d
Sylkaron <pabloberton@sfr.fr>
2021-10-20 16:55:02 +0200

    Unit et Commodity

--- Measurement.py
+++ Measurement.py
@@ -1,12 +1,11 @@
-import Describable
+import Describable, Commodity, Indicator
 
-class Measurement:
-    def __init__(self, year: int, value: float, timeperiodld: int, timeperiodDesc: str):
+class Measurement(Describable):
+    def __init__(self, id: int, year: int, value: float, timeperiodld: int, timeperiodDesc: str,
+                    commodity: Commodity, indicator: Indicator):
         self.year = year
         self.value = value
         self.timeperiodld = timeperiodld
         self.timeperiodDesc = timeperiodDesc
 
-    def measurement(self, id: int, year: int, value: float, timeperiodld: int, timeperiodDesc: str,
-                    commodity: Commodity, indicator: Indicator ):
 
