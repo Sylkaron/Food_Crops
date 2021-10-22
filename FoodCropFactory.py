77e4c1aae6b5110992b07adb4c7852a2abf84eaa
maxim <maxim.mangematin--mathey@mines-ales.org>
2021-10-22 15:47:11 +0200

    Fin seance vandredi

--- FoodCropFactory.py
+++ FoodCropFactory.py
@@ -4,34 +4,74 @@ from Measurement import *
 
 class FoodCropFactory:
 
-    def __init__(self, unitsRegistry: dict, indicatorsRegistry: dict, commodityRegistry: dict):
-        self.__unitsRegistry = unitsRegistry
-        self.__indicatorsRegistry = indicatorsRegistry
-        self.__commodityRegistry = commodityRegistry
+    def __init__(self,):
+        self.__unitsRegistry = dict()
+        self.__indicatorsRegistry = dict()
+        self.__commodityRegistry = dict()
 
     def createVolume(self, id:int):
-        return Volume(id)
+        if id not in self.__unitsRegistry :
+            U = Volume(id)
+            self.__unitsRegistry[id] = U
+            return U
+        else :
+            return self.__unitsRegistry[id]
 
     def createPrice(self, id:int):
-        return Price(id)
+        if id not in self.__unitsRegistry :
+            U = Price(id)
+            self.__unitsRegistry[id] = U
+            return U
+        else :
+            return self.__unitsRegistry[id]
 
     def createWeight(self, id:int, weight:float):
-        return Weight(id,weight)
+        if id not in self.__unitsRegistry :
+            U = Weight(id, weight)
+            self.__unitsRegistry[id] = U
+            return U
+        else :
+            return self.__unitsRegistry[id]
 
     def createSurface(self, id:int):
-        return Surface(id)
+        if id not in self.__unitsRegistry :
+            U = Surface(id)
+            self.__unitsRegistry[id] = U
+            return U
+        else :
+            return self.__unitsRegistry[id]
 
     def createCount(self,id:int, what:str):
-        return Count(id, what)
+        if id not in self.__unitsRegistry :
+            U = Count(id, what)
+            self.__unitsRegistry[id] = U
+            return U
+        else :
+            return self.__unitsRegistry[id]
 
     def createRatio(self, id:int):
-        return Ratio(id)
+        if id not in self.__unitsRegistry :
+            U = Ratio(id)
+            self.__unitsRegistry[id] = U
+            return U
+        else :
+            return self.__unitsRegistry[id]
 
     def createCommodity(self, group:CommodityGroup, id:int, name:str):
-        return Commodity(group, id, name)
+        if id not in self.__commodityRegistry :
+            C = Commodity(group, id, name)
+            self.__commodityRegistry[id] = C
+            return C
+        else :
+            return self.__commodityRegistry[id]
 
     def createIndicator(self, id:int, frequency:int, freqDesc:str, geogLocation:str, indicatorGroup:IndicatorGroup, unit:Unit ):
-        return Indicator(id, frequency, freqDesc, geogLocation, indicatorGroup, unit)
+        if id not in self.__indicatorsRegistry :
+            Indic = Indicator(id, frequency, freqDesc, geogLocation, indicatorGroup, unit)
+            self.indicatorRegistry[id] = Indic
+            retur Indic
+        else :
+            return self.__indicatorsRegistry[id]
 
     def createMeasurement(self, id:int, year:int, value:float, timeperiodId:int, timeperiodDesc:str, commodity:Commodity, indicator:Indicator):
         return Measurement(id, year, value, timeperiodId, timeperiodDesc, commodity, indicator)
--- FoodCropsDataset.py
+++ FoodCropsDataset.py
@@ -1,16 +1,60 @@
 import pandas
 
 class FoodCropsDataset:
+
     def __init__(self):
+        self.F = FoodCropFactory()
+        self.commodityGroupMeasurementIndex = dict()
+        self.indicatorGroupMesurementIndex = dict()
+        self.locationMeasurementIndex = dict()
+        self.unitMesurementIndex = dict()
 
     def load(self, datasetPath: str):
         dataframe = pandas.read_csv(datasetPath)
-        F = FoodCropFactory()
         for index, row in dataframe.iterrows():
-            Prix = row["SC_Group_ID"] * 1000
-            descPrix = row["SC_Group_Desc"]
-            CommodityID =  row["SC_GroupCommod_ID"]
-            CommodityDesc = row["SC_GroupCommod_Desc"]
+            IndicatorID = row["SC_Group_ID"] * 1000
+            IndicatorDesc = row["SC_Group_Desc"]
+            groupCommodityID =  row["SC_GroupCommod_ID"]
+            groupCommodityDesc = row["SC_GroupCommod_Desc"]
+            GeographyID = row["SC_Geography_ID"]
+            SortOrder = row["SortOrder"]
+            GeographyDesc = row["SC_GeographyIndented_Desc"]
+            Commodity = row["SC_Commodity_ID"]
+            CommodityDesc = row["SC_Commodity_Desc"]
+            AttributeID = row["SC_Attribute_ID"]
+            AttributeDesc = row["SC_Attribute_Desc"]
+            UnitID = row["SC_Unit_ID"]
+            UnitDesc = row["SC_Unit_Desc"]
+            YearID = row["Year_ID"]
+            FreqID = row["SC_Frequency_ID"]
+            FreqDesc = row["SC_Frequency_Desc"]
+            TimePID = row["Timeperiod_ID"]
+            TimePDesc = row["Timeperiod_Desc"]
+            Amount = row["Amount"]
+
+            if "per" or "ratio"  in UnitDesc.lower():
+                U = F.createRatio(UnitID)
+            elif "dollar" or "euro" in UnitDesc.lower():
+                U = F.createPrice(UnitID)
+            elif "liter" or "Bushel" or "gallon" in UnitDesc.lower():
+                U = F.createVolume(UnitID)
+            elif "ton" in UnitDesc.lower():
+                U = F.createWeight(UnitID, UnitDesc)
+            elif "acre" in UnitDesc.lower():
+                U = F.createSurface(UnitID)
+            else :
+                U = F.createCount(UnitID,UnitDesc)
+
+            I = F.createIndicator(IndicatorID, FreqID, FreqDesc, GeographyDesc, groupCommodityID, U)
+
+            G = F.createCommodity(groupCommodityID, groupCommodityDesc)
+
+            M = F.createMeasurement(index, YearID, Amount, timeperiodId, timeperiodDesc, G, I)
+
+            self.unitMesurementIndex[index] = U
+            self.locationMeasurementIndex[index] = GeographyDesc
+            self.commodityGroupMeasurementIndex[index] = G
+            self.indicatorGroupMesurementIndex[index] = I
 
     def findMeasurements(self, commodityType: CommodityType = None, indicatorGroup: IndicatorGroup = None,
                          geographicalLocation: str = None, unit: Unit = None):
